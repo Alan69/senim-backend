c8660b0fe06397f643ff069914bb6e5d385fd01d
Alan69 <gameg1676@gmail.com>
2024-10-30 15:43:05 +0500

    fix

--- payments/admin.py
+++ payments/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
-from .models import FetchedEmailData
-# Register your models here.
+# from django.contrib import admin
+# from .models import FetchedEmailData
+# # Register your models here.
 
-admin.site.register(FetchedEmailData)
+# admin.site.register(FetchedEmailData)
