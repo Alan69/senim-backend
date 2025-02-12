932c0bde107be65e25118b5828569c68e31b238e
Alan69 <gameg1676@gmail.com>
2025-02-12 13:03:27 +0500

    fix

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import PasswordResetForm
+from django.utils.html import format_html
 from .models import User, Region
 from .forms import UserCreationForm, UserChangeForm
 
@@ -7,7 +9,7 @@ class UserAdmin(BaseUserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
 
-    list_display = ('username', 'balance', 'first_name', 'last_name', 'region', 'school', 'email', 'phone_number', 'is_student', 'is_teacher', 'is_principal', 'is_staff', 'is_active', 'is_superuser')
+    list_display = ('username', 'balance', 'first_name', 'last_name', 'region', 'school', 'email', 'phone_number', 'is_student', 'is_teacher', 'is_principal', 'is_staff', 'is_active', 'is_superuser', 'password_reset_button')
     list_filter = ('is_student', 'is_teacher', 'is_principal', 'is_staff', 'is_active', 'is_superuser', 'region')
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
@@ -23,6 +25,30 @@ class UserAdmin(BaseUserAdmin):
     search_fields = ('username', 'first_name', 'last_name')
     ordering = ('username',)
     filter_horizontal = ('groups', 'user_permissions',)
+
+    def password_reset_button(self, obj):
+        return format_html(
+            '<a class="button" href="{}?next={}" style="background-color: #447e9b; padding: 5px 10px; color: white; text-decoration: none; border-radius: 4px;">Reset Password</a>',
+            f"/admin/password_reset/",
+            f"/admin/accounts/user/{obj.pk}/change/"
+        )
+    password_reset_button.short_description = 'Reset Password'
+    password_reset_button.allow_tags = True
+
+    actions = ['send_password_reset_email']
+
+    def send_password_reset_email(self, request, queryset):
+        for user in queryset:
+            form = PasswordResetForm({'email': user.email})
+            if form.is_valid():
+                form.save(
+                    request=request,
+                    use_https=request.is_secure(),
+                    subject_template_name='registration/password_reset_subject.txt',
+                    email_template_name='registration/password_reset_email.html',
+                )
+        self.message_user(request, f"Password reset emails sent to {queryset.count()} users")
+    send_password_reset_email.short_description = "Send password reset email to selected users"
 
 admin.site.register(User, UserAdmin)
 
