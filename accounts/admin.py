13f52ac398dd69c9591a1908b450a493fc6a43b7
Alan69 <gameg1676@gmail.com>
2025-02-12 13:09:22 +0500

    fix

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,8 +6,7 @@ from .models import User, Region
 from .forms import UserCreationForm, UserChangeForm
 
 class UserAdmin(BaseUserAdmin):
-    form = UserChangeForm
-    add_form = UserCreationForm
+    model = User
 
     list_display = ('username', 'balance', 'first_name', 'last_name', 'region', 'school', 'email', 'phone_number', 'is_student', 'is_teacher', 'is_principal', 'is_staff', 'is_active', 'is_superuser', 'password_reset_button')
     list_filter = ('is_student', 'is_teacher', 'is_principal', 'is_staff', 'is_active', 'is_superuser', 'region')
@@ -25,30 +24,6 @@ class UserAdmin(BaseUserAdmin):
     search_fields = ('username', 'first_name', 'last_name')
     ordering = ('username',)
     filter_horizontal = ('groups', 'user_permissions',)
-
-    def password_reset_button(self, obj):
-        return format_html(
-            '<a class="button" href="{}?next={}" style="background-color: #447e9b; padding: 5px 10px; color: white; text-decoration: none; border-radius: 4px;">Reset Password</a>',
-            f"/admin/password_reset/",
-            f"/admin/accounts/user/{obj.pk}/change/"
-        )
-    password_reset_button.short_description = 'Reset Password'
-    password_reset_button.allow_tags = True
-
-    actions = ['send_password_reset_email']
-
-    def send_password_reset_email(self, request, queryset):
-        for user in queryset:
-            form = PasswordResetForm({'email': user.email})
-            if form.is_valid():
-                form.save(
-                    request=request,
-                    use_https=request.is_secure(),
-                    subject_template_name='registration/password_reset_subject.txt',
-                    email_template_name='registration/password_reset_email.html',
-                )
-        self.message_user(request, f"Password reset emails sent to {queryset.count()} users")
-    send_password_reset_email.short_description = "Send password reset email to selected users"
 
 admin.site.register(User, UserAdmin)
 
